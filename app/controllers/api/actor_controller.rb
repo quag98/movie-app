b9cd1a1932fb312252ea6dcc285d55ca6e92aa2c
David Quaggin-Smith <quagginsmith@gmail.com>
2021-03-03 10:40:51 -0600

    adds w3 homework

--- app/controllers/api/actor_controller.rb
+++ app/controllers/api/actor_controller.rb
@@ -5,6 +5,42 @@ class Api::ActorController < ApplicationController
     render "actor.param.json.jb"
   end
 
-  def actor_puts_action
+  def index
+    @actor = Actor.all
+    render "index.json.jb"
+  end
+
+  def show
+    id_search = params["id"]
+    @actor = Actor.find_by(id: id_search)
+    render "show.json.jb"
+  end
+
+  def create
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+    )
+    @actor.save
+    render "show.json.jb"
+  end
+
+  def update
+    actor_id = params["id"]
+    @actor = Actor.find_by(id: actor_id)
+
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    actor_id = params["id"]
+    @actor = Actor.find_by(id: actor_id)
+    @actor.destroy
+    render json: { message: "bye bye actor" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   namespace :api do
     get "/actor_single_path" => "actor#actor_single_action"
     get "/actor_single_path/:param" => "actor#actor_single_action"
-    get "action_puts_path" => "actor#actor_puts_action"
+    get "/action_puts_path" => "actor#actor_puts_action"
+    get "/actor" => "actor#index"
+    get "/actor/:id" => "actor#show"
+    post "/actor" => "actor#create"
+    patch "/actor/:id" => "actor#update"
+    delete "/actor/:id" => "actor#destroy"
   end
 end
